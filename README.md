9f1a281eb209ae63a4720d6211c46ecf9ca6b1af
Andrew <andrewsmith1025@gmail.com>
2022-10-10 12:40:25 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# wear-base-template
---
+++ app/src/main/java/com/example/hello_world/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.hello_world
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.hello_world.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
